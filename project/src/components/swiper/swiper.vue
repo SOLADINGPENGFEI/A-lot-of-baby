c03bd8c321c9a27e4af5897f853384add1590ec0
fei <724613865@qq.com>
2019-07-08 19:57:41 +0800

    首页排版完成

---
+++ project/src/components/swiper/swiper.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="lunBo">
+     
+     <swiper :indicator-dots="indicatorDots"
+  :autoplay="autoplay" :interval="interval" :duration="duration" :circular="circular">
+  <block>
+    <swiper-item>
+       <div>111</div>
+    </swiper-item>
+        <swiper-item>
+       <div>222</div>
+    </swiper-item>
+        <swiper-item>
+       <div>333</div>
+    </swiper-item>
+  </block>
+</swiper>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+    circular:true,
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
+        }
+    },
+    methods:{
+    }
+
+}
+</script>
+
+<style scoped lang="scss">
+     .lunBo{
+         width: 100%;
+         height:150px;
+         background:skyblue;
+         margin-top:5px;
+     }
+</style>
--- project/src/pages/index/index.vue
+++ project/src/pages/index/index.vue
@@ -1,126 +1,139 @@
 <template>
-  <div @click="clickHandle">
 
-    <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
-      <img class="userinfo-avatar" src="/static/images/user.png" background-size="cover" />
+ <scroll-view scroll-y>
+  <div class="wrap">
+              <!-- 首页搜索导航跳转搜索页面 -->
+            <div class="inPut">
+                <div class="bigInput">
+                     <img src="/static/images/search.png" alt="">
+                     <div>请输入商品名称</div>
+                </div>
+            </div>      
+              <!-- 导航组件 -->
+              <Nava></Nava>  
+              <!-- 轮播图组件 -->
+              <swiper-a></swiper-a>
+              <!-- 首页Banner广告图 -->
+            <div class="banner">
+                <div class="banOne">
+                    <img src="" />
+                </div>
+                <div class="banTwo">
+                    <img class="one" src="" />
+                    <img class="two" src="" />
+                </div>
+            </div>
 
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
-      </div>
-    </div>
-
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
-      </div>
-    </div>
-
-    <form class="form-container">
-      <input type="text" class="form-control" :value="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
-
-    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
-
-    <div class="all">
-        <div class="left">
-        </div>
-        <div class="right">
-        </div>
-    </div>
+            <div class="bigimg">
+               <img src="" alt="">
+            </div>
+             <!--精选好物Dl组件  --> 
+            <ban-ner></ban-ner>
+            <!-- 底部加载loading组件 -->
+            <loa-ding></loa-ding>
   </div>
+  </scroll-view>
 </template>
 
 <script>
-import card from '@/components/card'
+import Nava from "@/components/nav/nav"; //头部导航组件
+import SwiperA from "@/components/swiper/swiper"; // 轮播组件
+import banNer from '@/components/banner/banner'  // 精选好物Dl组件
+import loaDing from '@/components/loading/loading' // 底部加载loading组件
 
 export default {
-  data () {
-    return {
-      motto: 'Hello miniprograme',
-      userInfo: {
-        nickName: 'mpvue',
-        avatarUrl: 'http://mpvue.com/assets/logo.png'
-      }
-    }
+  data() {
+    return {};
   },
-
   components: {
-    card
-  },
-
-  methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      if (mpvuePlatform === 'wx') {
-        mpvue.switchTab({ url })
-      } else {
-        mpvue.navigateTo({ url })
-      }
-    },
-    clickHandle (ev) {
-      console.log('clickHandle:', ev)
-      // throw {message: 'custom test'}
-    }
-  },
-
-  created () {
-    // let app = getApp()
+    Nava, //挂载导航组件
+    SwiperA, // 挂载轮播图组件
+    banNer, // 挂载精选好物Dl组件
+    loaDing  // 挂载底部loding加载组件
   }
-}
+};
 </script>
 
-<style scoped>
-.userinfo {
+
+<style scoped lang="scss">
+.wrap {
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
+  padding:0 6px;
+  box-sizing: border-box;
+}
+.inPut {
+  width: 100%;
+  height: 44px;
+  display: flex;
+  justify-content: center;
   align-items: center;
+  .bigInput {
+    width:98%;
+    height: 35px;
+    background: #f6f6f6;
+    display: flex;
+    align-items: center;
+    padding: 0 10px;
+    box-sizing: border-box;
+    border-radius: 5px;
+    img {
+      width: 15px;
+      height: 15px;
+      color: #a9adb1;
+    }
+    div {
+      flex: 1;
+      padding: 0 6px;
+      box-sizing: border-box;
+      color: #a9adb1;
+    }
+  }
 }
+.nav {
+  height: 100px;
+  background: red;
+}
+.banner{
+  width:100%;
+  height:220px;
+  background:#fff;
+  display: flex;
+  margin-top:10px;
+    .banOne{
+      width:40%;
+      background:green;
+      margin:0 3px;
+      border-radius: 5px;
+    }
+    .banTwo{
+      width:59%;
+      display: flex;
+      flex-direction: column;
+      .one{
+        width:100%;
+        height:48%;
+        background:salmon;
+        margin-bottom: 3px;
+        border-radius: 5px;
+      }
+      .two{
+        width:100%;
+        height:50%;
+        background:sandybrown;
+        border-radius: 5px;
+      }
+    }
+}
+   .bigimg{
+      width:100%;
+      height:100px;
+      background:saddlebrown;
+      margin-top:5px;
+      border-radius: 10px;
+    }
 
-.userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
-}
 
-.userinfo-nickname {
-  color: #aaa;
-}
-
-.usermotto {
-  margin-top: 150px;
-}
-
-.form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
-}
-.all{
-  width:7.5rem;
-  height:1rem;
-  background-color:blue;
-}
-.all:after{
-  display:block;
-  content:'';
-  clear:both;
-}
-.left{
-  float:left;
-  width:3rem;
-  height:1rem;
-  background-color:red;
-}
-
-.right{
-  float:left;
-  width:4.5rem;
-  height:1rem;
-  background-color:green;
-}
 </style>
